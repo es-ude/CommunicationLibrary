67a29fbabb962cde5fc3fbdabcb8aacf4f1c2f64
Lukas Einhaus <lukas.einhaus@t-online.de>
2018-01-15 16:26:59 +0100

    added a simple mutex

--- src/lib/source/Mutex.c
+++ src/lib/source/Mutex.c
@@ -6,3 +6,34 @@ Mutex_init(Mutex *self)
 {
   self->key = NULL;
 }
+
+inline bool
+Mutex_isLocked(Mutex *self)
+{
+  return (self->key != NULL);
+}
+
+bool
+Mutex_lockWithKey(Mutex *self, void *key)
+{
+  if (self->key != NULL)
+    {
+      return false;
+    }
+  else
+    {
+      self->key = key;
+      return true;
+    }
+}
+
+bool
+Mutex_unlockWithKey(Mutex *self, void *key)
+{
+  if (self->key == key)
+    {
+      self->key = NULL;
+      return true;
+    }
+  return false;
+}
