aa8c94bfae469484179fb537aa257c37995a4d1f
Einhaus, Lukas <lukas.einhaus@stud.uni-due.de>
2018-02-10 11:55:08 +0100

    IM-49 first draft for new SPIDevice struct

--- lib/include/SPI.h
+++ lib/include/SPI.h
@@ -5,6 +5,7 @@
 
 typedef struct SPIMessage SPIMessage;
 typedef struct SPIDevice SPIDevice;
+typedef struct SPI SPI;
 
 struct SPIMessage {
 	uint8_t length;
@@ -12,11 +13,21 @@ struct SPIMessage {
 	uint8_t* incoming_data;
 };
 
-struct SPIDevice {
-	void (*transferSync) (const SPIDevice *self, const SPIMessage *message);
-	void (*transferAsync) (const SPIDevice *self, const SPIMessage *message);
+struct SPI {
+	void (*transferSync) (const SPIDevice *self, 
+			const SPIMessage *message,
+			uint8_t *slave_select_line);
+	void (*transferAsync) (const SPIDevice *self, 
+			const SPIMessage *message,
+			uint8_t *slave_select_line);
 	void (*init) (SPIDevice *self);
 };
+
+struct SPIDevice {
+	SPI *hw_interface;
+	uint8_t *slave_select_line;
+};
+
 
 /**
  * The transfer functions write outgoing_data contained inside message
@@ -24,11 +35,11 @@ struct SPIDevice {
  * points to.
  */
 static void SPI_transferSync(const SPIDevice *self, const SPIMessage *data){
-	self->transferSync(self, data);
+	self->interface->transferSync(self, data, self->slave_select_line);
 }
 
 static void SPI_transferAsync(const SPIDevice *self, const SPIMessage *data) {
-	self->transferAsync(self, data);
+	self->interface->transferAsync(self, data, self->slave_select_line);
 }
 
 
