3989d2826cbae29de2a6660b89f83f0c8a855ef2
Einhaus, Lukas <lukas.einhaus@stud.uni-due.de>
2018-02-13 10:49:02 +0100

    IM-51 extended NetworkHardware interface with getters

--- lib/include/NetworkHardware.h
+++ lib/include/NetworkHardware.h
@@ -3,18 +3,26 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "lib/include/Message.h"
 
 typedef struct NetworkHardware NetworkHardware;
 
 struct NetworkHardware {
-    void (*setAddress16Bit) (NetworkHardware *self, uint16_t address);
-    void (*setAddress64Bit) (NetworkHardware *self, uint8_t* address);
-    void (*setPayload) (NetworkHardware *self, uint8_t* buffer, size_t size);
-    void (*setPAN) (NetworkHardware *self, uint16_t pan_id);
-    void (*receive) (NetworkHardware *self, Message *received_msg);
-    void (*send) (NetworkHardware *self);
-    void (*init) (NetworkHardware *self);
+  void (*setAddress16Bit) (NetworkHardware *self, uint16_t address);
+  void (*setAddress64Bit) (NetworkHardware *self, uint8_t* address);
+  void (*setPayload) (NetworkHardware *self, uint8_t* buffer, size_t size);
+  void (*setPAN) (NetworkHardware *self, uint16_t pan_id);
+  void (*associate) (NetworkHardware *self);
+  void (*receive) (NetworkHardware *self, Message *received_msg);
+  void (*send) (NetworkHardware *self);
+  void (*init) (NetworkHardware *self);
+  void (*destroy) (NetworkHardware *self);
+  bool (*isAssociated) (NetworkHardware *self);
+  uint16_t (*getAddress16Bit) (NetworkHardware *self);
+  const uint8_t *(*getAddress64Bit) (NetworkHardware *self);
+  const uint8_t *(*getPayload) (NetworkHardware *self);
+  uint16_t (*getPayloadSize) (NetworkHardware *self);
 };
 
 static inline void NetworkHardware_init(NetworkHardware *hardware) {
