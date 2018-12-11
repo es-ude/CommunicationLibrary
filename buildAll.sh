b41db55c7ab8886d7d7862a27ba5c7acda653940
Lukas Einhaus <lukas.einhaus@stud.uni-due.de>
2018-12-11 16:56:05 +0100

    make file copied by buildAll.sh non-camel-case

--- buildAll.sh
+++ buildAll.sh
@@ -4,7 +4,7 @@ function build_and_zip {
     bazel build :CommunicationModuleZip --config=$1
     if [ ! -z "$OUTPUT_DIR" ]; then
         mkdir -p "$OUTPUT_DIR"
-        cp -f bazel-genfiles/CommunicationModule.zip "$OUTPUT_DIR"/
+        cp -f bazel-genfiles/CommunicationModule.zip "$OUTPUT_DIR"/communicationmodule.zip
     fi
 }
 
