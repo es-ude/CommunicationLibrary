87194f44a7fe415ecfff232f7f831aa97e5fd4e2
Lukas Einhaus <lukas.einhaus@stud.uni-due.de>
2018-12-11 15:20:31 +0100

    changed buildAll.sh to execute only one build

--- buildAll.sh
+++ buildAll.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 function build_and_zip {
-    bazel build :CommunicationModule_$1_zip --config=$1
+    bazel build :CommunicationModuleZip --config=$1
     if [ ! -z "$OUTPUT_DIR" ]; then
         mkdir -p "$OUTPUT_DIR"
-        cp -f bazel-genfiles/CommunicationModule_$1.zip "$OUTPUT_DIR"/
+        cp -f bazel-genfiles/CommunicationModule.zip "$OUTPUT_DIR"/
     fi
 }
 
 build_and_zip atmega32u4
-build_and_zip atmega328p
-build_and_zip atmega64
