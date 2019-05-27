0e13a65faaecd2f17795309438c8724c4892eba1
adf694g <alwyn.burger@uni-due.de>
2019-05-27 12:47:20 +0200

    fixed build script

--- buildAll.sh
+++ buildAll.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-function build_and_zip {
-    bazel build :pkg
-    if [ ! -z "$OUTPUT_DIR" ]; then
-        mkdir -p "$OUTPUT_DIR"/"$1"/
-        cp -f bazel-bin/pkg.tar.gz "$OUTPUT_DIR"/"$1"/"$1".tar.gz
-    fi
-}
-
-# build_and_zip $1 native
-build_and_zip $1
+bazel build pkg
+mkdir -p "$OUTPUT_DIR"/"$1"/
+cp -f bazel-bin/pkg.tar.gz "$OUTPUT_DIR"/"$1"/"$1"-"$2".tar.gz
